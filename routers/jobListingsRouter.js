3ba32d9fb6c3ebcf0a74ec512ce90817d887dc93
kaisheng1989 <kaisheng1989@gmail.com>
2023-08-30 18:59:11 +0800

    job listing by categories

--- routers/jobListingsRouter.js
+++ routers/jobListingsRouter.js
@@ -13,11 +13,15 @@ class JobListingsRouter {
       this.controller.getAllCategory.bind(this.controller)
     );
     router.get(
+      "/:jobCategoryId",
+      this.controller.getOneCategory.bind(this.controller)
+    );
+    router.get(
       "/:companyId",
       this.controller.getOneCompanyListing.bind(this.controller)
     );
 
-    router.get("/:jobCategoryId", this.controller.getOneCategory.bind(this.controller));
+    
     // to add in protected route w jwt, eg
     // router.post(
     //   "/",
