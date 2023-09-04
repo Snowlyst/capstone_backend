06a3fa4cc3e5a7c424cd3bd17a125047c8f274f0
kaisheng1989 <kaisheng1989@gmail.com>
2023-09-04 09:18:28 +0800

    backend

--- db/models/location.js
+++ db/models/location.js
@@ -3,7 +3,7 @@ const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Location extends Model {
     static associate(models) {
-      this.hasMany(models.job_listing, {foreignKey:"jobListingId"});
+      this.hasMany(models.job_listing);
     }
   }
   Location.init(
